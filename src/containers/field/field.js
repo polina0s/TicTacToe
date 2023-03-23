d116ba588ebdb72b7a52cdf162ae7f601e02b92f
polina0s <polina.osintseva000@gmail.com>
2023-03-23 17:27:11 +0400

    add new method for field

--- src/containers/field/field.js
+++ src/containers/field/field.js
@@ -1,13 +1,21 @@
+import { makeid } from '../../lib/makeid';
 export class Field {
   constructor(onCellClick) {
     this.cells = document.querySelectorAll('#cell');
 
+    this.addDataIdForCells();
     this.addListenerForCells(onCellClick);
   }
 
   addListenerForCells(onCellClick) {
     this.cells.forEach((el) => {
       el.addEventListener('click', onCellClick);
+    });
+  }
+
+  addDataIdForCells() {
+    this.cells.forEach((el) => {
+      el.setAttribute('data-id', makeid());
     });
   }
 }
